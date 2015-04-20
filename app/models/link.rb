1113ef22106ca6b79f8da036f6dbbfae2fb3402f
Chris C Cerami <chrisccerami@gmail.com>
2015-04-19 23:26:36 -0400

    Fix fresh? method in Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ActiveRecord::Base
   FRESHNESS_PERIOD = 7 * 24 * 60 * 60 # 1 week
 
   def fresh?
-    Time.now - self.updated_at > FRESHNESS_PERIOD
+    Time.now - self.updated_at < FRESHNESS_PERIOD
   end
 
   def self.stales
