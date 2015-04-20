6041c3c90fa318f80a623dd46f7d49a34871a1e8
Chris C Cerami <chrisccerami@gmail.com>
2015-04-19 22:33:22 -0400

    Refactor Crawler

--- app/models/crawler.rb
+++ app/models/crawler.rb
@@ -8,13 +8,17 @@ class Crawler
       rescue
         puts "Error encountered while crawling"
       else
-        hrefs = html.css('a')
-        hrefs.each do |href|
-          url = href.attributes["href"].value
-          new_link = Link.find_or_create_by(url: url)
-          new_link.update_attributes(html: html)
-        end
+        collect_links(html)
       end
+    end
+  end
+
+  def collect_links(html)
+    hrefs = html.css('a')
+    hrefs.each do |href|
+      url = href.attributes["href"].value
+      new_link = Link.find_or_create_by(url: url)
+      new_link.update_attributes(html: html)
     end
   end
 end
